c50613356ee486c4a70b6e8c550c6a41bf0bbb8b
NamYoung Kim <095201a@naver.com>
2022-03-25 14:02:44 +0900

    main/resources
    1. log4j.xml 변경
    2. log4j properties 파일 추가
    3. my-batis-config.xml 파일 추가
    
    WEB-INF
    
    1. servlet-context.xml 변경
    2. root-context.xml 변경
    3. web.xml 변경
    
    pom.xml 메이븐 의존 설정 완료

--- src/main/java/itwill/helljava/dao/AccountDAO.java
+++ src/main/java/itwill/helljava/dao/AccountDAO.java
@@ -1,5 +1,20 @@
 package itwill.helljava.dao;
 
+import java.util.List;
+
+import itwill.helljava.dto.Account;
+import itwill.helljava.dto.Member;
+
 public interface AccountDAO {
 
+	void addAcount(Member member);
+
+	void removeAccount(int member_no);
+
+	void modifyAccount(Member member);
+
+	Account getMemberAccount(Member member);
+
+	List<Account> getAccountList();
+	
 }
