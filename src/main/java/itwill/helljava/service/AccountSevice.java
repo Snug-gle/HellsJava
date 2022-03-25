0438b8c874aeed3f63fb1e7d1eaf22f4ca1bc7f0
NamYoung Kim <095201a@naver.com>
2022-03-25 12:22:37 +0900

    service 추상 메서드 선언

--- src/main/java/itwill/helljava/service/AccountSevice.java
+++ src/main/java/itwill/helljava/service/AccountSevice.java
@@ -1,5 +1,19 @@
 package itwill.helljava.service;
 
+import java.util.List;
+
+import itwill.helljava.dto.Account;
+import itwill.helljava.dto.Member;
+
 public interface AccountSevice {
 
+	void addAcount(Member member);
+
+	void removeAccount(int member_no);
+
+	void modifyAccount(Member member);
+
+	Account getMemberAccount(Member member);
+
+	List<Account> getAccountList();
 }
--- src/main/java/itwill/helljava/service/PayService.java
+++ src/main/java/itwill/helljava/service/PayService.java
@@ -1,5 +1,18 @@
 package itwill.helljava.service;
 
+import java.util.List;
+import java.util.Map;
+
+import itwill.helljava.dto.Pay;
+
 public interface PayService {
 
+	void addPay(Pay pay);
+	void modifyPay(Pay pay);
+	//void removePay(int pay_no); // 결제 데이터를 삭제할 일은 없을듯..
+	int getPayCount();
+	Pay getPay(int pay_no);
+	List<Pay> getPayList(Map<String, Object> map);
+	
+	
 }
