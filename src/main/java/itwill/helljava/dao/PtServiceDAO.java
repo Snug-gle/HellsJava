6c68fd769847cd002e087ce4200c60ac7e536a7c
JI <JI@DESKTOP-IPKC005>
2022-03-25 15:27:23 +0900

    dao commit

--- src/main/java/itwill/helljava/dao/PtServiceDAO.java
+++ src/main/java/itwill/helljava/dao/PtServiceDAO.java
@@ -1,5 +1,14 @@
 package itwill.helljava.dao;
 
-public interface PtServiceDAO {
+import java.util.List;
+import java.util.Map;
 
+import itwill.helljava.dto.PtService;
+
+public interface PtServiceDAO {
+	int insertPtService(PtService ptService);
+	int updatePtService(PtService ptService);
+	int selectPtServiceCount(int pt_service_sortation);
+	PtService selectPtService(int pt_service_no, int pt_service_sortation);
+	List<PtService> selectPtServiceList(Map<String, Object> map, int pt_service_sortation);
 }
