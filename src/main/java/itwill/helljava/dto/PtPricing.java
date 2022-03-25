fa1ea195ab8bc8dc59ad2a15b6d9c9c1a96801ce
JI <JI@DESKTOP-IPKC005>
2022-03-25 11:01:49 +0900

    dto commit

---
+++ src/main/java/itwill/helljava/dto/PtPricing.java
@@ -0,0 +1,54 @@
+package itwill.helljava.dto;
+
+public class PtPricing {
+	private int pt_pricing_no;
+	private int trainer_no;
+	private String pt_pricing_round;
+	private int pt_pricing_price;
+	private int pt_pricing_status;
+	
+	public PtPricing() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getPt_pricing_no() {
+		return pt_pricing_no;
+	}
+
+	public void setPt_pricing_no(int pt_pricing_no) {
+		this.pt_pricing_no = pt_pricing_no;
+	}
+
+	public int getTrainer_no() {
+		return trainer_no;
+	}
+
+	public void setTrainer_no(int trainer_no) {
+		this.trainer_no = trainer_no;
+	}
+
+	public String getPt_pricing_round() {
+		return pt_pricing_round;
+	}
+
+	public void setPt_pricing_round(String pt_pricing_round) {
+		this.pt_pricing_round = pt_pricing_round;
+	}
+
+	public int getPt_pricing_price() {
+		return pt_pricing_price;
+	}
+
+	public void setPt_pricing_price(int pt_pricing_price) {
+		this.pt_pricing_price = pt_pricing_price;
+	}
+
+	public int getPt_pricing_status() {
+		return pt_pricing_status;
+	}
+
+	public void setPt_pricing_status(int pt_pricing_status) {
+		this.pt_pricing_status = pt_pricing_status;
+	}
+	
+}
--- src/main/java/itwill/helljava/dto/Trainer.java
+++ src/main/java/itwill/helljava/dto/Trainer.java
@@ -1,5 +1,63 @@
 package itwill.helljava.dto;
 
 public class Trainer {
+	private int trainer_no;
+	private int member_no;
+	private String trainer_profile_img;
+	private String trainer_address1;
+	private String trainer_addresss2;
+	private String trainer_zip;
+	
+	public Trainer() {
+		// TODO Auto-generated constructor stub
+	}
 
+	public int getTrainer_no() {
+		return trainer_no;
+	}
+
+	public void setTrainer_no(int trainer_no) {
+		this.trainer_no = trainer_no;
+	}
+
+	public int getMember_no() {
+		return member_no;
+	}
+
+	public void setMember_no(int member_no) {
+		this.member_no = member_no;
+	}
+
+	public String getTrainer_profile_img() {
+		return trainer_profile_img;
+	}
+
+	public void setTrainer_profile_img(String trainer_profile_img) {
+		this.trainer_profile_img = trainer_profile_img;
+	}
+
+	public String getTrainer_address1() {
+		return trainer_address1;
+	}
+
+	public void setTrainer_address1(String trainer_address1) {
+		this.trainer_address1 = trainer_address1;
+	}
+
+	public String getTrainer_addresss2() {
+		return trainer_addresss2;
+	}
+
+	public void setTrainer_addresss2(String trainer_addresss2) {
+		this.trainer_addresss2 = trainer_addresss2;
+	}
+
+	public String getTrainer_zip() {
+		return trainer_zip;
+	}
+
+	public void setTrainer_zip(String trainer_zip) {
+		this.trainer_zip = trainer_zip;
+	}
+	
 }
