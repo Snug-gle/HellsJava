40ddf2edebc84867fbb13c253e8fde874778c56f
JI <JI@DESKTOP-IPKC005>
2022-03-25 15:57:57 +0900

    service impl commit

--- src/main/java/itwill/helljava/service/PtPricingServiceImpl.java
+++ src/main/java/itwill/helljava/service/PtPricingServiceImpl.java
@@ -1,5 +1,37 @@
 package itwill.helljava.service;
 
-public class PtPricingServiceImpl implements PtPricingService{
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import itwill.helljava.dao.PtPricingDAO;
+import itwill.helljava.dto.PtPricing;
+
+@Service
+public class PtPricingServiceImpl implements PtPricingService {
+	@Autowired
+	private PtPricingDAO ptPricingDAO;
+
+	@Override
+	public void addPtPricing(PtPricing ptPricing) {
+		ptPricingDAO.insertPtPricing(ptPricing);
+	}
+
+	@Override
+	public void modifyPtPricing(PtPricing ptPricing) {
+		ptPricingDAO.updatePtPricing(ptPricing);
+	}
+
+	@Override
+	public PtPricing getPtPring(int pt_pricing_no) {
+		return ptPricingDAO.selectPtPring(pt_pricing_no);
+	}
+
+	@Override
+	public List<PtPricing> getPtPricingList() {
+		return ptPricingDAO.selectPtPricingList();
+	}
+	
+	
 }
--- src/main/java/itwill/helljava/service/TrainerServiceImpl.java
+++ src/main/java/itwill/helljava/service/TrainerServiceImpl.java
@@ -1,5 +1,48 @@
 package itwill.helljava.service;
 
-public class TrainerServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import itwill.helljava.dao.TrainerDAO;
+import itwill.helljava.dto.Trainer;
+
+@Service
+public class TrainerServiceImpl implements TrainerService {
+	@Autowired
+	private TrainerDAO trainerDAO;
+
+	@Override
+	public void addTrainer(Trainer trainer) {
+		trainerDAO.insertTrainer(trainer);
+	}
+
+	@Override
+	public void modifyTrainer(Trainer trainer) {
+		trainerDAO.updateTrainer(trainer);
+	}
+
+	@Override
+	public void removeTrainer(Trainer trainer) {
+		trainerDAO.deleteTrainer(trainer);
+	}
+
+	@Override
+	public int getTrainerCount() {
+		return trainerDAO.selectTrainerCount();
+	}
+
+	@Override
+	public Trainer getTrainer(int trainer_no) {
+		return trainerDAO.selectTrainer(trainer_no);
+	}
+
+	@Override
+	public List<Trainer> getTrainerList() {
+		return trainerDAO.selectTrainerList();
+	}
+	
+	
+	
 }
