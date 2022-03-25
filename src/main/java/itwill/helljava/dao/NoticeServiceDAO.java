7c9f0e2cb6e91c1cd5e9c0e0666a60600c86fa3b
NamYoung Kim <095201a@naver.com>
2022-03-25 15:28:04 +0900

    DAO 추상메서드 선언

--- src/main/java/itwill/helljava/dao/NoticeServiceDAO.java
+++ src/main/java/itwill/helljava/dao/NoticeServiceDAO.java
@@ -1,5 +1,21 @@
 package itwill.helljava.dao;
 
+import java.util.List;
+import java.util.Map;
+
+import itwill.helljava.dto.NoticeService;
+
 public interface NoticeServiceDAO {
 
+	int insertNoticeService(NoticeService noticeService);
+
+	int updateNoticeService(NoticeService noticeService);
+
+	int selectNoticeServiceCount(int pt_service_sortation);
+
+	// 합친 DB이기 때문에 구분하려면 혹시 몰라서 글 구분 매개변수도 추가함.
+	NoticeService selectNoticeService(int pt_service_no, int pt_service_sortation);
+
+	// 마찬가지로 글 구분 매개변수도 추가함
+	List<NoticeService> selectNoticeServiceList(Map<String, Object> map1, int pt_service_sortation);
 }
