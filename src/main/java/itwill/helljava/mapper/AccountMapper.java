6b21eaaa7b7f65ac7f52bc0d00c7d7a69a7f1b77
NamYoung Kim <095201a@naver.com>
2022-03-25 15:33:06 +0900

    Mapper 추상 메서드 선언

--- src/main/java/itwill/helljava/mapper/AccountMapper.java
+++ src/main/java/itwill/helljava/mapper/AccountMapper.java
@@ -1,5 +1,19 @@
 package itwill.helljava.mapper;
 
+import java.util.List;
+
+import itwill.helljava.dto.Account;
+import itwill.helljava.dto.Member;
+
 public interface AccountMapper {
 
+	int insertAcount(Member member);
+
+	int deleteAccount(int member_no);
+
+	int updateAccount(Member member);
+
+	Account selectAccount(Member member);
+
+	List<Account> selectAccountList();
 }
