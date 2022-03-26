28bdabe85155eaa870e1a81684d8db3a7447ade7
Sanghoon Park <nonsleep79@gmail.com>
2022-03-27 01:04:20 +0900

    noticeController 수정 및 체크 필요

--- src/main/java/itwill/helljava/controller/NoticeController.java
+++ src/main/java/itwill/helljava/controller/NoticeController.java
@@ -1,5 +1,8 @@
 package itwill.helljava.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import itwill.helljava.dto.NoticeService;
 import itwill.helljava.service.NoticeServiceService;
+import itwill.helljava.util.Pager;
 
 @Controller
 @RequestMapping("/notice")
@@ -21,10 +25,23 @@ public class NoticeController {
 	@Autowired 
 	NoticeServiceService noticeServiceService;
 		
-	//공지사항 리스트 출력 요청 처리 메소드
-	@RequestMapping("/list")
-	public String List(@ModelAttribute NoticeService noticeService, Model model){
-		model.addAttribute("noticeList", noticeServiceService.getNoticeServiceList(noticeService, 0));
+	//공지사항 리스트 출력 처리 메소드
+	@RequestMapping(value = "/list" ,method = RequestMethod.GET)
+	public String List(@ModelAttribute NoticeService noticeService, Model model, @RequestParam(defaultValue = "1") int pageNum){
+		//테이블에 저장된 모든 게시글의 갯수를 검색하여 반환받아 저장
+		int totalBoard = noticeServiceService.getNoticeServiceCount(0);
+		int pageSize = 5; //한 페이지에 출력될 게시글의 갯수 저장
+		int blockSize = 10; //한 페이지 블럭에 출력될 페이지 번호의 갯수 저장
+		
+		//페이징 처리 관련 값을 제공하는 Pager 클래스로 객체를 생성하여 저장
+		Pager pager = new Pager(pageNum, totalBoard, pageSize, blockSize);
+		
+		//Service 클래스의 메소드 호출을 위한 Map 객체 생성
+		Map<String,	Object> pagerMap = new HashMap<String, Object>();
+		pagerMap.put("starRow", pager.getStartRow());
+		pagerMap.put("endRow", pager.getEndRow());
+		
+		model.addAttribute("noticeList", noticeServiceService.getNoticeServiceList(pagerMap, 0));
 		return "board/notice_list";
 	}
 	
@@ -33,7 +50,7 @@ public class NoticeController {
 	public String view(@RequestParam int notice_service_no , Model model) {
 		model.addAttribute("notice", noticeServiceService.getNoticeService(notice_service_no, 0));
 		
-		return "redirect:board/notice_view";
+		return "redirect:/board/notice_view";
 	}
 	
 	//공지사항 입력 페이지 출력 요청 처리 메소드
@@ -45,9 +62,9 @@ public class NoticeController {
 	
 	//공지사항 입력 사항 저장 요청 처리 메소드
 	@RequestMapping( value = "/write" , method = RequestMethod.POST)
-	public String write(Model model) {
+	public String write(@ModelAttribute Model model) {
 		noticeServiceService.addNoticeService(null);
-		return "redirect:board/notice_list";
+		return "redirect:/board/notice_list";
 	}
 	
 	//공지사항 수정 페이지 출력 요청 처리 메소드
@@ -60,14 +77,14 @@ public class NoticeController {
 	//공지사항 수정 사항 저장 요청 처리 메소드
 	@RequestMapping(value =  "/modify" , method = RequestMethod.POST)
 	public String modify(@ModelAttribute NoticeService noticeService , HttpSession session) throws Exception {
-		this.noticeServiceService.modifyNoticeService(noticeService);
-		return "redirect:board/notice_list";
+		noticeServiceService.modifyNoticeService(noticeService);
+		return "redirect:/board/notice_list";
 	}
 
 	//공지사항 삭제
 	@RequestMapping( value = "/remove/{num}" , method = RequestMethod.POST)
 	public String remove(@PathVariable int num, @ModelAttribute NoticeService noticeService) throws Exception{
-		noticeService.modifyNoticeService(noticeService);
+		noticeServiceService.modifyNoticeService(noticeService);
 		return "redirect:/notice/list";
 	}
 	 
