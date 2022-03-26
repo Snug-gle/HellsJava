c2cdb575838fcb324539be5b833646c84b5ad755
Sanghoon Park <nonsleep79@gmail.com>
2022-03-26 17:02:56 +0900

    notice controller 메소드 수정, notice_view 페이지 수정
    * 공지사항 리스트 출력, 삭제 관련 메소드 이슈

--- src/main/java/itwill/helljava/controller/NoticeController.java
+++ src/main/java/itwill/helljava/controller/NoticeController.java
@@ -1,33 +1,74 @@
 package itwill.helljava.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import itwill.helljava.dto.NoticeService;
+import itwill.helljava.service.NoticeServiceService;
 
 @Controller
 @RequestMapping("/notice")
 public class NoticeController {
-
-	@RequestMapping("/list")
-	public String list() {
+	
+	@Autowired 
+	NoticeServiceService noticeServiceService;
 		
+	//공지사항 리스트 출력 요청 처리 메소드
+	@RequestMapping("/list")
+	public String List(@ModelAttribute NoticeService noticeService, Model model){
+		model.addAttribute("noticeList", noticeServiceService.getNoticeServiceList(noticeService, 0));
 		return "board/notice_list";
 	}
-
-	@RequestMapping( value = "/write" ,method = RequestMethod.GET)
+	
+	//공지사항 세부 사항 출력 요청 처리 메소드
+	@RequestMapping(value = "/view", method = RequestMethod.GET) 
+	public String view(@RequestParam int notice_service_no , Model model) {
+		model.addAttribute("notice", noticeServiceService.getNoticeService(notice_service_no, 0));
+		
+		return "redirect:board/notice_view";
+	}
+	
+	//공지사항 입력 페이지 출력 요청 처리 메소드
+	@RequestMapping("/write")
 	public String write() {
 			
 		return "board/notice_write";
 	}
-
+	
+	//공지사항 입력 사항 저장 요청 처리 메소드
+	@RequestMapping( value = "/write" , method = RequestMethod.POST)
+	public String write(Model model) {
+		noticeServiceService.addNoticeService(null);
+		return "redirect:board/notice_list";
+	}
+	
+	//공지사항 수정 페이지 출력 요청 처리 메소드
 	@RequestMapping(value =  "/modify" , method = RequestMethod.GET)
-	public String modify() {
-				                                       
+	public String modify(@RequestParam int notice_service_no , Model model) throws Exception {
+		model.addAttribute("notice" , noticeServiceService.getNoticeService(notice_service_no, 0));
 		return "board/notice_modify";
 	}
+	
+	//공지사항 수정 사항 저장 요청 처리 메소드
+	@RequestMapping(value =  "/modify" , method = RequestMethod.POST)
+	public String modify(@ModelAttribute NoticeService noticeService , HttpSession session) throws Exception {
+		this.noticeServiceService.modifyNoticeService(noticeService);
+		return "redirect:board/notice_list";
+	}
 
-
-	/*
-	 * @RequestMapping("/remove") public St                  
-	 */
+	//공지사항 삭제
+	@RequestMapping( value = "/remove/{num}" , method = RequestMethod.POST)
+	public String remove(@PathVariable int num, @ModelAttribute NoticeService noticeService) throws Exception{
+		noticeService.modifyNoticeService(noticeService);
+		return "redirect:/notice/list";
+	}
+	 
 }
---
+++ src/main/webapp/WEB-INF/views/board/notice_view.jsp
@@ -0,0 +1,49 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>    
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<%-- <link rel=stylesheet href="<c:url value="/css/user.css"/>" type="text/css"> --%>
+<script language="JavaScript">
+function noticeRemove(num) {
+	if (confirm("정말로 삭제 하시겠습니까?") ) {
+		location.href='<c:url value="/notice/remove"/>?num='+num;
+	}
+}                                                                                                              
+</script>
+</head>
+<body>
+<br>
+<h3>공지사항</h3>
+	  
+ <table border="0" cellpadding="0" cellspacing="1" width="590" bgcolor="BBBBBB">
+ 	<tr>
+ 		<td width=100 align=center height="22">${notice.service.no }</td>
+ 		<td width=100 align=center height="22">${notice.service.title }</td>
+ 		<td width=100 align=center height="22">${notice.service.date }</td>
+ 		<td width=100 align=center height="22">${notice.service.writer }</td>
+ 		<td width=100 align=center height="22">${notice.service.hits }</td>
+	</tr>
+	<tr>
+		<td width=500 style="padding-left:10px;">
+			${notice.service.content}
+		</td>			
+	</tr>
+ </table>
+  <br>
+  <table>
+	  <tr>
+		<td align=center>
+		<input type="button" value="목록" onClick="location.href='<c:url value="/notice/list"/>';"> 
+		<c:if test="${loginUserinfo.status==9 }">
+		<input type="button" value="수정" onClick="location.href='<c:url value="/notice/modify"/>?notice.service.no=${notice.service.no }';">
+		<input type="button" value="삭제" onClick="noticeRemove('${notice.service.no }');">
+		</c:if>
+			</td>
+		  </tr>
+	  </table>
+
+</body>
+</html>
