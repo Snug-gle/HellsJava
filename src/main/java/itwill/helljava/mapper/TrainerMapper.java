b7dfd2932c22fc5fd181e2560140ac43fb20edf9
NamYoung Kim <095201a@naver.com>
2022-03-26 19:32:42 +0900

    SQL 변경 사항 적용

--- src/main/java/itwill/helljava/mapper/TrainerMapper.java
+++ src/main/java/itwill/helljava/mapper/TrainerMapper.java
@@ -1,13 +1,13 @@
 package itwill.helljava.mapper;
 
-import java.util.List;
-
 import itwill.helljava.dto.Trainer;
 
 public interface TrainerMapper {
+	
 	int insertTrainer(Trainer trainer);
+
 	int updateTrainer(Trainer trainer);
-	int selectTrainerCount();
+
 	Trainer selectTrainer(int trainer_no);
-	List<Trainer> selectTrainerList();
+
 }
