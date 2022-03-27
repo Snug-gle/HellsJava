bca044a1789213f7b8b16383d240a502d3b962f2
NamYoung Kim <095201a@naver.com>
2022-03-28 00:42:01 +0900

    update

--- src/main/java/itwill/helljava/mapper/PayMapper.java
+++ src/main/java/itwill/helljava/mapper/PayMapper.java
@@ -11,8 +11,8 @@ public interface PayMapper {
 
 	// void removePay(int pay_no); // 결제 데이터를 삭제할 일은 없을듯..
 	int selectPayCount(int member_no);
-
+	
 	Pay selectPay(int pay_no);
-
+	
 	List<Pay> selectPayList(Map<String, Object> map, int member_no);
 }
