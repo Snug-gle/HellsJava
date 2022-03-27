11a217321a8c78a04276a7489249db3ea7031bde
NamYoung Kim <095201a@naver.com>
2022-03-27 17:10:11 +0900

    xml 파일들 변경

--- src/test/java/itwill/helljava/controller/AccountServiceTest.java
+++ src/test/java/itwill/helljava/controller/AccountServiceTest.java
@@ -14,25 +14,33 @@ import itwill.helljava.service.AccountSevice;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/*.xml"})
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/**/*.xml" })
 public class AccountServiceTest {
 
-	private static final Logger logger=LoggerFactory.getLogger(AccountServiceTest.class);
+	private static final Logger logger = LoggerFactory.getLogger(AccountServiceTest.class);
 
 	@Autowired
 	private AccountSevice accountSevice;
-	
+
+	/*
+	 * @Test public void testaddAccount() { Account account = new Account();
+	 * account.setMember_no(24); account.setAccount_bank(1);
+	 * account.setAccount_number(12003); account.setAccount_pw("123456");
+	 * 
+	 * accountSevice.addAccount(account);
+	 * 
+	 * logger.info("저장 완료!");
+	 * 
+	 * }
+	 */
+
 	@Test
-	public void testaddAccount() {
-		Account account = new Account();
-		account.setMember_no(30);
-		account.setAccount_bank(1);
-		account.setAccount_number(123123);
-		account.setAccount_pw("123456");
-		
-		accountSevice.addAccount(account);
-		
-		logger.info("저장 완료!");
-		
+	public void testgetAccount() {
+		logger.info("하이");
+		Account account = accountSevice.getMemberAccount(30);
+
+		logger.info("30 번 계좌 정보 : " + account);
+
 	}
+
 }
