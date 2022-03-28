8ead278cd545b8cd83eff3f0c359ae955176b30c
NamYoung Kim <095201a@naver.com>
2022-03-28 20:28:45 +0900

    enum 패키지 생성

---
+++ src/main/java/itwill/helljava/Enum/AccountEnum.java
@@ -0,0 +1,31 @@
+package itwill.helljava.Enum;
+
+public enum AccountEnum {
+	
+	국민(0), 신한(1), 하나(2), 농협(3), 우체국(4), 우리(5);
+	
+	private final int bank;
+	
+	private AccountEnum(final int bank) {
+		this.bank = bank;
+	}
+	
+	public static AccountEnum of(final int number) {
+		
+		switch (number) {
+			
+			case 0 : 
+				return 국민;
+			case 1 :
+				return 신한;
+			case 2 :
+				return 하나;
+			case 3 :
+				return 농협;
+			case 4 :
+				return 우체국;
+			default :
+				return 우리;		
+		}
+	}
+}
---
+++ src/main/java/itwill/helljava/Enum/MemberEnum.java
@@ -0,0 +1,30 @@
+package itwill.helljava.Enum;
+
+public enum MemberEnum {
+
+	RESIGN(0), NORMAL(1), PRE_TRAINER(2), TRAINER(3), MANAGER(9);
+
+	private final int memberGrade;
+
+	private MemberEnum(int memberGrade) {
+		this.memberGrade = memberGrade;
+	}
+
+	public static MemberEnum of(final int number) {
+
+		switch (number) {
+			case 0:
+				return RESIGN;
+			case 2:
+				return PRE_TRAINER;
+			case 3:
+				return TRAINER;
+			case 9:
+				return MANAGER;
+	
+			default:
+				return NORMAL;
+			}
+
+	}
+}
