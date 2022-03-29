56288b566d59c66182d417432013c34955d986b6
JI <JI@DESKTOP-IPKC005>
2022-03-29 11:06:33 +0900

    NoticeServiceStatusEnum 수정

--- src/main/java/itwill/helljava/Enum/NoticeServiceStatusEnum.java
+++ src/main/java/itwill/helljava/Enum/NoticeServiceStatusEnum.java
@@ -1,7 +1,7 @@
 package itwill.helljava.Enum;
 
 public enum NoticeServiceStatusEnum {
-	삭제글(1), 일반글(2), 문의미답변(3), 문의답변(4);
+	삭제글(0), 일반글(1), 문의미답변(2), 문의답변(3);
 	
 	private int noticeStatus;
 	
@@ -12,11 +12,11 @@ public enum NoticeServiceStatusEnum {
 	public static NoticeServiceStatusEnum of(final int number) {
 		
 		switch (number) {
-			case 1:
+			case 0:
 				return 삭제글;
-			case 3:
+			case 2:
 				return 문의미답변;
-			case 4:
+			case 3:
 				return 문의답변;
 			default:
 				return 일반글;
