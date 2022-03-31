6c1cb7df040abf57ef12a19483ffea8cdc080583
NamYoung Kim <095201a@naver.com>
2022-03-31 16:10:05 +0900

    회원 관리 관련 대거 수정

--- src/main/java/itwill/helljava/controller/AdminController.java
+++ src/main/java/itwill/helljava/controller/AdminController.java
@@ -6,13 +6,15 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import itwill.helljava.Enum.NoticeServiceSortationEnum;
-import itwill.helljava.Enum.NoticeServiceStatusEnum;
+import itwill.helljava.dto.Member;
+import itwill.helljava.service.MemberService;
 import itwill.helljava.service.NoticeServiceService;
 import itwill.helljava.util.Pager;
 
@@ -21,12 +23,14 @@ public class AdminController {
 	
 	@Autowired
 	private NoticeServiceService noticeServiceService;
-
-	@RequestMapping(value = "admin/userList", method = RequestMethod.GET)
-	public String userList() {
-		return "admin/admin_userList";
-	}
 	
+	@Autowired
+	private MemberService memberService;
+
+	/*
+	 * @RequestMapping(value = "admin/userList", method = RequestMethod.GET) public
+	 * String userList() { return "admin/admin_userList"; }
+	 */
 	@RequestMapping(value = "admin/trainerList", method = RequestMethod.GET)
 	public String trainerList() {
 		return "admin/admin_trainerList";
@@ -42,7 +46,7 @@ public class AdminController {
 	 * public String questionList() { return "admin/admin_questionList"; }
 	 */
 	
-	// 공지사항 리스트 출력 메서드 (Map 형태로 반환)
+	// 1:1 문의 리스트 출력 메서드 (Map 형태로 반환)
 	@RequestMapping(value = "admin/questionList",method = RequestMethod.GET)
 	@ResponseBody
 	public Map<String, Object> restQAList(@RequestParam(defaultValue = "1") int pageNum, Model model){
@@ -69,6 +73,46 @@ public class AdminController {
 		returnMap.put("pager", pager);
 		
 		return returnMap;
+	}
+	
+	// 회원 리스트 요청
+	@RequestMapping(value = "admin/userList", method = RequestMethod.GET)
+	public String memberList(@RequestParam(defaultValue ="1") int pageNum, Model model) {
+		
+		Map<String, Object> searchMap = new HashMap<String, Object>();
+		
+		int totalBoard = memberService.getMemberListCount(searchMap);
+		
+		int pageSize = 5;
+		int blockSize = 10;
+		
+		Pager pager = new Pager(pageNum, totalBoard, pageSize, blockSize);
+
+		Map<String, Object> returnMap = new HashMap<String, Object>();
+		
+		returnMap.put("startRow", pager.getStartRow());
+		returnMap.put("endRow", pager.getEndRow());
+		
+		model.addAttribute("memberList", memberService.getMemberList(returnMap));
+
+		return "/admin/admin_userList";
+	}
+	
+	// 회원 상태 변경 요청 -> 상태값을 전달받자.
+	@RequestMapping(value = "/admin/userStatusModify", method = RequestMethod.GET)
+	public String memberStatusModify(@RequestParam int memberNo, @RequestParam int memberStatus) {
+	
+		// 회원 객체 가져오고
+		Member modifyMember =memberService.getMember(memberNo);
+		
+		// 회원 객체에 setter로 인자 값 넣고
+		modifyMember.setMemberNo(memberNo);
+		modifyMember.setMemberStatus(memberStatus);
+		
+		// 바뀐 속성 값 가진 객체 태워 보내기 
+		memberService.modifyMember(modifyMember);
+		
+		return "redirect:/admin/userList";
 	}
 	
 }
