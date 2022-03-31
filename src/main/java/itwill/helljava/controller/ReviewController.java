0656a86947aef3a38c42697be711f03e700db5a0
Sanghoon Park <nonsleep79@gmail.com>
2022-03-31 21:33:00 +0900

    commit

--- src/main/java/itwill/helljava/controller/ReviewController.java
+++ src/main/java/itwill/helljava/controller/ReviewController.java
@@ -1,14 +1,36 @@
 package itwill.helljava.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import itwill.helljava.service.PtServiceService;
 
 @Controller
 @RequestMapping("/review")
 public class ReviewController {
 
+	@Autowired
+	private PtServiceService ptServiceService;
+	
 	@RequestMapping("/write")
-	public String reviewWrite() {
-		return "board"
+	public String reviewWrite(Model model) {
+		
+		//평점 옵션
+		Map ratingOptions = new HashMap();
+		ratingOptions.put(0, "☆☆☆☆☆");
+		ratingOptions.put(1, "★☆☆☆☆");
+		ratingOptions.put(2, "★★☆☆☆");
+		ratingOptions.put(3, "★★★☆☆");
+		ratingOptions.put(4, "★★★★☆");
+		ratingOptions.put(5, "★★★★★");
+		
+		model.addAttribute("ratingOptions", ratingOptions);
+		
+		return "board/review_write";
 	}
 }
