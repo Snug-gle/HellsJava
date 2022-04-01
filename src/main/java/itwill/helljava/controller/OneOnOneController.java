c58d615b67465aafd3855648d872940f0db39b17
Sanghoon Park <nonsleep79@gmail.com>
2022-04-01 10:20:18 +0900

    oOo컨트롤러 수정 커밋
    수정

--- src/main/java/itwill/helljava/controller/OneOnOneController.java
+++ src/main/java/itwill/helljava/controller/OneOnOneController.java
@@ -10,15 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import itwill.helljava.dto.Member;
+import itwill.helljava.dto.NoticeService;
 import itwill.helljava.dto.PtService;
-import itwill.helljava.service.PtServiceService;
+import itwill.helljava.service.NoticeServiceService;
 
 @Controller
 @RequestMapping("/oOo")
 public class OneOnOneController {
 
 	@Autowired
-	private PtServiceService ptServiceService;
+	private NoticeServiceService noticeServiceService;
 	
 	//1:1 문의 내역
 	@RequestMapping("/list")
@@ -34,18 +35,17 @@ public class OneOnOneController {
 	
 	//1:1 문의 등록 포스트 방식 요청 처리 메소드
 	@RequestMapping(value =  "/write" , method = RequestMethod.POST)
-	public String register(@ModelAttribute PtService ptService, HttpSession session) { 
+	public String register(@ModelAttribute NoticeService noticeService, HttpSession session) { 
 		
-		ptService.setMemberNo(((Member)session.getAttribute("loginUserinfo")).getMemberNo());
-		ptServiceService.addPtService(ptService);
+		noticeService.setMemberNo(((Member)session.getAttribute("loginUserinfo")).getMemberNo());
+		noticeServiceService.addNoticeService(noticeService);
 		return "board/oOo_list";
 	}
 	
 	// 글번호를 받아 1:1 문의 삭제 요청
-	// =>삭제 메소드를 만드는게 더 나을듯 함
 	@RequestMapping(value = "/remove/{num}" ,method = RequestMethod.GET)
 	public String remove(@ModelAttribute PtService ptService ,@PathVariable int num) {
-		ptServiceService.modifyPtServiceGood(num);
+		noticeServiceService.removeNoticeService(num);
 		return "board/oOo_list";
 	}
 }
