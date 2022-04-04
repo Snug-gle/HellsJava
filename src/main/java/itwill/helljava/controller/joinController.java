7044be68ad695c4434cffd1a6e482605d33e37ec
NamYoung Kim <095201a@naver.com>
2022-04-04 23:54:55 +0900

    Exception Handler 추가

--- src/main/java/itwill/helljava/controller/joinController.java
+++ src/main/java/itwill/helljava/controller/joinController.java
@@ -14,8 +14,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import itwill.helljava.dto.Member;
 import itwill.helljava.exception.MemberExistsException;
+import itwill.helljava.exception.PhoneExistsException;
 import itwill.helljava.service.MemberService;
-
 
 @Controller
 @RequestMapping("/user")
@@ -23,38 +23,47 @@ public class joinController {
 
 	@Autowired
 	MemberService memberService;
-	
+
 	// 회원가입 페이지 요청 메서드
 	@RequestMapping(value = "/join_form", method = RequestMethod.GET)
 	public String join() {
 		return "user/join_form";
 	}
-	
+
 	// 회원가입 처리 핸들러메서드
 	@RequestMapping(value = "/join", method = RequestMethod.POST)
-	public String join(@ModelAttribute Member member, Model model, HttpServletRequest request) throws MemberExistsException {
-		
-		String phone = request.getParameter("member_phone1") +"-"+ request.getParameter("member_phone2") +"-"+ request.getParameter("member_phone3");
-		
+	public String join(@ModelAttribute Member member, Model model, HttpServletRequest request)
+			throws MemberExistsException, PhoneExistsException {
+
+		String phone = request.getParameter("member_phone1") + "-" + request.getParameter("member_phone2") + "-"
+				+ request.getParameter("member_phone3");
+
 		member.setMemberPhone(phone);
 		memberService.addMember(member);
 		return "user/login/login_form";
 	}
-	
+
 	@ExceptionHandler(value = MemberExistsException.class)
 	public String exceptionHandler(MemberExistsException exception, Model model) {
 		model.addAttribute("message", exception.getMessage());
 		model.addAttribute("member", exception.getMember());
 		return "user/join_form";
 	}
-	
+
+	@ExceptionHandler(value = PhoneExistsException.class)
+	public String exceptionHandler(PhoneExistsException exception, Model model) {
+		model.addAttribute("message", exception.getMessage());
+		model.addAttribute("member", exception.getMember());
+		return "user/join_form";
+	}
+
 	@RequestMapping(value = "/user/idcheck", method = RequestMethod.GET)
-    @ResponseBody
-    public int idCheck(@RequestParam("id") String id){
-		
-		System.out.println("전달받은 id:"+id);
-        int cnt = memberService.idCheck(id);
-        System.out.println("전달받은 cnt:"+cnt);
-        return cnt;
-    }
+	@ResponseBody
+	public int idCheck(@RequestParam("id") String id) {
+
+		System.out.println("전달받은 id:" + id);
+		int cnt = memberService.idCheck(id);
+		System.out.println("전달받은 cnt:" + cnt);
+		return cnt;
+	}
 }
