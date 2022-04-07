feaecc257de9aae843e0a1c7a83f81c5af6ea83f
Sunghun Kim <redmango1446@gmail.com>
2022-04-07 18:48:58 +0900

    관리자 1:1 검색 페이징 및 내 리뷰 보기

--- src/main/java/itwill/helljava/controller/ReviewController.java
+++ src/main/java/itwill/helljava/controller/ReviewController.java
@@ -3,16 +3,25 @@ package itwill.helljava.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import itwill.helljava.Enum.NoticeServiceSortationEnum;
+import itwill.helljava.Enum.PtServiceSortationEnum;
+import itwill.helljava.Enum.PtServiceStatusEnum;
+import itwill.helljava.dto.Member;
 import itwill.helljava.dto.NoticeService;
 import itwill.helljava.dto.PtService;
 import itwill.helljava.service.PtServiceService;
+import itwill.helljava.util.Pager;
 
 @Controller
 @RequestMapping("/review" )
@@ -21,15 +30,65 @@ public class ReviewController {
 	@Autowired
 	private PtServiceService ptServiceService;
 	
-	//list 페이지 요청 처리 메소드
-	@RequestMapping("/list")
-	public String searchList() {
+	//내가 쓴 리뷰 보기 목록 리스트
+	@RequestMapping(value = "/list",method = RequestMethod.GET)
+	public String searchList(@RequestParam(defaultValue = "1") int pageNum, HttpSession session, Model model){
+		
+		if (session.getAttribute("loginUserinfo") != null) {
+			
+		}
+
+		Map<String, Object> searchMap = new HashMap<String, Object>();
+		
+		searchMap.put("pt_service_sortation", PtServiceSortationEnum.리뷰.getValue());
+		searchMap.put("member_no", ((Member) session.getAttribute("loginUserinfo")).getMemberNo());
+		
+		int totalBoard = ptServiceService.getPtServiceCount(searchMap);
+		int pageSize = 5; //한 페이지에 출력될 게시글의 갯수 저장
+		int blockSize = 10; //한 페이지 블럭에 출력될 페이지 번호의 갯수 저장
+		
+		//페이징 처리 관련 값을 제공하는 Pager 클래스로 객체를 생성하여 저장
+		Pager pager = new Pager(pageNum, totalBoard, pageSize, blockSize);
+		
+		//Service 클래스의 메소드 호출을 위한 Map 객체 생성
+		Map<String,	Object> returnMap = new HashMap<String, Object>();
+		returnMap.put("startRow", pager.getStartRow());
+		returnMap.put("endRow", pager.getEndRow());
+		returnMap.put("pt_service_sortation", PtServiceSortationEnum.리뷰.getValue());
+		returnMap.put("pt_service_status", PtServiceStatusEnum.일반리뷰.getValue());
+		returnMap.put("member_no", ((Member) session.getAttribute("loginUserinfo")).getMemberNo());
+		
+		model.addAttribute("reviewrList",ptServiceService.getPtServiceList(returnMap));
+		model.addAttribute("pager",pager);
+
 		return "board/review_list";
 	}
+	
+	// //내가 쓴 리뷰 보기 목록 리스트 =>삭제
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public String memberStatusModify(@RequestParam int pt_service_no, @RequestParam int pt_service_status) {
+	
+		PtService ptdelete = ptServiceService.getPtService(pt_service_no);
+		
+		ptdelete.setPtServiceNo(pt_service_no);
+		ptdelete.setPtServiceStatus(pt_service_status);
+		
+		ptServiceService.modifyPtService(ptdelete);
+		
+		return "redirect:/review/List";
+	}
+	
+	
+	
+	
+	
 	//리뷰 작성 페이지 요청 처리 메소드
 	@RequestMapping(value = "/write" , method = RequestMethod.GET)
-	public String reviewWrite() {
+	public String reviewWrite(HttpSession session) {
 		
+		if (session.getAttribute("loginUserinfo") != null) {
+			
+		}
 	
 		return "board/review_write";
 	}
