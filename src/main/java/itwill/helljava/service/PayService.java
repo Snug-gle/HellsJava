c3bed24fd5e5efb5da65932dcfd98f3c228d42de
NamYoung Kim <095201a@naver.com>
2022-04-12 23:45:15 +0900

    자동 결제 관련 sql controller jsp 수정 및 캐시 잔액 예외 처리

--- src/main/java/itwill/helljava/service/PayService.java
+++ src/main/java/itwill/helljava/service/PayService.java
@@ -16,7 +16,7 @@ public interface PayService {
 	// void removePay(int pay_no); // 결제 데이터를 삭제할 일은 없을듯..
 	int getPayCount(int member_no);
 
-	Pay getPay(int pay_no);
+	Pay getPay(Map<String, Object> map);
 
 	List<Pay> getPayList(Map<String, Object> map, int member_no);
 
